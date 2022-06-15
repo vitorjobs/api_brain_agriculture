4c58af60d8771606ed8ccf576e1414cbe0232fa6
Vitor Guedes <vitorjobs@gmail.com>
2022-06-15 19:19:18 -0300

    :books: chore: Model Cultura Estruturacao

---
+++ src/models/Culturas.ts
@@ -0,0 +1,24 @@
+import { Entity, Column, CreateDateColumn, PrimaryColumn } from "typeorm";
+import {v4 as uuid} from "uuid"
+
+@Entity("cultura")
+export class Producer {
+  
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  nome: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+
+  constructor(){
+    if(!this.id){
+      this.id = uuid()
+    }
+  }
+}
