23cbc4f54db20edd5244775a5b97a87df470fd7d
Vitor Guedes <vitorjobs@gmail.com>
2022-06-15 19:13:57 -0300

    :books: chore: Model Enderecoo Estruturacao

---
+++ src/models/Enderecos.ts
@@ -0,0 +1,27 @@
+import { Entity, Column, CreateDateColumn, PrimaryColumn } from "typeorm";
+import {v4 as uuid} from "uuid"
+
+@Entity("endereco")
+export class Producer {
+  
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  cidade: string;
+
+  @Column()
+  estado: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+
+  constructor(){
+    if(!this.id){
+      this.id = uuid()
+    }
+  }
+}
