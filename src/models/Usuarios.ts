7c06bdad95bf7e49457ba49e5ff84571d05b9a61
Vitor Guedes <vitorjobs@gmail.com>
2022-06-15 18:46:24 -0300

    :books: chore: Model Usuario Estruturacao

---
+++ src/models/Usuarios.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, CreateDateColumn, PrimaryColumn } from "typeorm";
+import {v4 as uuid} from "uuid"
+
+@Entity("usuario")
+export class Producer {
+  
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+  isUnique: true
+  
+  @Column()
+  senha: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+
+  constructor(){
+    if(!this.id){
+      this.id = uuid()
+    }
+  }
+}
