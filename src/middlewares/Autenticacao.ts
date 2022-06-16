8bdb06874e3d204ac679568b9a5b51728238adab
Vitor Guedes <vitorjobs@gmail.com>
2022-06-16 01:05:07 -0300

    :sparkles: feat: Combo Middlware Login e Perfil de usuário

---
+++ src/middlewares/Autenticacao.ts
@@ -0,0 +1,43 @@
+// ARQUIVO PARA VALIDAR A AUTENTICAÇÃO DO USUÁRIO
+
+import { Request, Response, NextFunction } from "express"
+import {verify} from "jsonwebtoken"
+
+interface IPayload {
+  sub: string
+}   
+
+export function Autenticacao(
+    request: Request, 
+    response: Response, 
+    next:NextFunction)
+    
+  {
+ 
+  // RECEBER O TOKEN VIA BEARER TOKEN E PELO HEADERS
+  const authToken = request.headers.authorization
+
+  // VÁLIDA O TOKEN PREENCHIDO
+  if(!authToken){
+    return response.status(401).json(" ATENÇÃO !!! TOKEN NÃO PREENCHIDO")
+  }
+
+  // VERIFICAR SE O TOKEN É VÁLIDO E DIVIDE A STRING COM O SPLIT
+  const [, token] = authToken.split(" ")
+
+  try {
+    const {sub} = verify(
+      token, 
+      "66cc811a6dcbf1b64c69edacbf53e53d"
+      ) as IPayload
+
+    request.user_id = sub
+
+    return next()
+    
+  } catch (error) {
+    return response.status(401).json("Falha na validão do TOKEN,")
+
+  }
+
+}
---
+++ src/middlewares/ValidacaoUsuarioAdm.ts
@@ -0,0 +1,26 @@
+// ARQUIVO PARA VALIDAR USUÁRIO COMO ADMINISTRADOR
+
+import { Request, Response, NextFunction } from "express"
+import { getCustomRepository } from "typeorm"
+import { RepositorioUsuario } from "../repositories/RepositorioUsuario"
+
+export async function ensureAdmin(request: Request, response: Response, next:NextFunction){
+  const {user_id} = request
+
+  try {
+    // VERIFICAR SE O USUÁRIO É ADMIN
+    const repositorioUsuario = getCustomRepository(RepositorioUsuario)
+    const {admin} = await repositorioUsuario.findOne(user_id)
+
+    if(admin){
+      return next()
+    }
+
+    return response.status(401).json({
+      error: "Usuário não autorizado"
+    })
+
+  } catch (error) {
+      return error  
+  }
+}
