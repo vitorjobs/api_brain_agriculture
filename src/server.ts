ede8dc4494ccbf534b5723981c1a980c79a8d547
Vitor Guedes <vitorjobs@gmail.com>
2022-06-15 23:43:39 -0300

    :bricks: ci: Arquivo server reestruturado Validação de erros Servidor

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,54 @@
 /**
  * 
- * Server Application
- *   
- **/
-import express from "express"
+ * GET - Server Application
+ * SET - JSON
+ * 
+ */
 require('dotenv/config')
+import "reflect-metadata"
+import "express-async-errors"
+import express, {Request, Response, NextFunction } from "express"
+import {router} from "./routes"
+import "./database"
 
-const server = express()
+const app = express()
 
-server.listen(process.env.SERVER_PORT, () =>{
-  console.log(`NodeJs Server Running on port: 3000`)
+/* Set Json */
+app.use(express.json())
+
+/* Setar Rotas */
+app.use(router)
+
+
+// Apresentar mensagens de retorno na tela
+app.use((
+  err: Error, 
+  request: Request, 
+  response: Response, 
+  next: NextFunction)=>
+  {
+    if(err instanceof Error){
+      return response.status(400).json({
+        error: err.message,
+      })
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error"
+    })
+  }
+)
+
+app.get("/terms", (request, response) =>{
+    return response.json({
+      Termos: "Termos de Serviço da Aplicação de Teste para vaga de NODEJS na NTT_DATA"
+    }),
+    console.log('cheguei auqi')
+  })
+
+
+/* Set Server Application */
+app.listen(process.env.SERVER_PORT, () =>{
+    console.log(`NodeJs Server Running on port: 3000`)
 })
-
-export {server}
