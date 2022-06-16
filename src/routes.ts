eee376c372d087c9e31391edaf9faeac54071015
Vitor Guedes <vitorjobs@gmail.com>
2022-06-15 23:46:14 -0300

    :sparkles: feat: Banco de dados estruturado

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import {Router} from "express"
+import {ControladorCriarUsuario} from "./controllers/ControladorCriarUsuario"
+
+const router = Router()
+
+// Controlador - USUÁRIOS
+const createUserController = new ControladorCriarUsuario()
+
+/*Rotas = USUÁRIOS */
+router.post("/usuario", createUserController.handle)
+
+export {router}
